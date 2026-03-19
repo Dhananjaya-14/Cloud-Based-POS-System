fc870fc637d683f430698b2a64252430458880f9
Nirmi De Zoysa <nirmidezoysa01@gmail.com>
2026-03-19 14:11:55 +0530

    fix: complete add new branch with adding a branch manager

--- Client/src/components/admin/Button.jsx
+++ Client/src/components/admin/Button.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-const Button = ({ label }) => {
+const Button = ({ label, onClick, type = "button", ...rest }) => {
   return (
     <button
+      type={type}
+      onClick={onClick}
+      {...rest}
+
       style={{
         padding: "10px 16px",
         background: "#3A4DBF",
