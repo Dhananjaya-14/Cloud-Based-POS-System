e268c876e02af11bdb6fb72f15a435ecae506054
Rahula <rahulabasnayaka2002@gmail.com>
2026-03-23 19:58:42 +0530

    completed customer API

--- Server/config/database.js
+++ Server/config/database.js
@@ -1,3 +1,19 @@
-// Database configuration for Neon DB
+import dotenv from "dotenv";
+import pkg from "pg";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default pool;
+
+
 
 
