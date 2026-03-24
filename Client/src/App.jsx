78912bd0b160ecee2a6427457644d092df29e589
Nirmi De Zoysa <nirmidezoysa01@gmail.com>
2026-03-25 02:24:37 +0530

    feat:connecting login api to the frontend

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import RegisterStep1 from './pages/RegisterStep1';
 import RegisterStep2 from './pages/RegisterStep2';
@@ -7,39 +7,146 @@ import RegisterStep3 from './pages/RegisterStep3';
 import BranchManagement from './pages/admin/BranchManagement';
 import AddUser from './pages/admin/AddUser';
 import EditUser from './pages/admin/EditUser';
-
-
 import BranchProfile from './pages/admin/branch_profile';
 import BranchProfileEdit from './pages/admin/branchProfileEdit';
 import UserManagement from './pages/admin/UserManagement';
-
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        {/* <Route path="/" element={<Navigate to="/register/step-1" />} /> */}
-        <Route path="/register/step-1" element={<RegisterStep1 />} />
-        <Route path="/register/step-2" element={<RegisterStep2 />} />
-        <Route path="/register/step-3" element={<RegisterStep3 />} />
-        
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register/step-1" element={<RegisterStep1 />} />
+      <Route path="/register/step-2" element={<RegisterStep2 />} />
+      <Route path="/register/step-3" element={<RegisterStep3 />} />
 
-        <Route path="/branches" element={<BranchManagement />} />
-        <Route path="/users" element={<UserManagement />} />
-        <Route path="/users/add" element={<AddUser />} />
-        <Route path="/users/:userId/edit" element={<EditUser/>}/>
-        <Route path="/branch_profile/:branchId" element={<BranchProfile />} />
-        <Route path="/branch_profile/:branchId/edit" element={<BranchProfileEdit />} />
+      <Route
+        path="/branches"
+        element={
+          <ProtectedRoute allowedRoles={[2]}>
+            <BranchManagement />
+          </ProtectedRoute>
+        }
+      />
 
+      <Route
+        path="/users"
+        element={
+          <ProtectedRoute allowedRoles={[2]}>
+            <UserManagement />
+          </ProtectedRoute>
+        }
+      />
 
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+      <Route
+        path="/users/add"
+        element={
+          <ProtectedRoute allowedRoles={[2]}>
+            <AddUser />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/users/:userId/edit"
+        element={
+          <ProtectedRoute allowedRoles={[2]}>
+            <EditUser />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/branch_profile/:branchId"
+        element={
+          <ProtectedRoute allowedRoles={[1, 2]}>
+            <BranchProfile />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/branch_profile/:branchId/edit"
+        element={
+          <ProtectedRoute allowedRoles={[2]}>
+            <BranchProfileEdit />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import Login from './pages/Login';
+// import RegisterStep1 from './pages/RegisterStep1';
+// import RegisterStep2 from './pages/RegisterStep2';
+// import RegisterStep3 from './pages/RegisterStep3';
+// import BranchManagement from './pages/admin/BranchManagement';
+// import AddUser from './pages/admin/AddUser';
+// import EditUser from './pages/admin/EditUser';
+
+
+// import BranchProfile from './pages/admin/branch_profile';
+// import BranchProfileEdit from './pages/admin/branchProfileEdit';
+// import UserManagement from './pages/admin/UserManagement';
+
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         {/* <Route path="/" element={<Navigate to="/register/step-1" />} /> */}
+//         <Route path="/register/step-1" element={<RegisterStep1 />} />
+//         <Route path="/register/step-2" element={<RegisterStep2 />} />
+//         <Route path="/register/step-3" element={<RegisterStep3 />} />
+        
+
+//         <Route path="/branches" element={<BranchManagement />} />
+//         <Route path="/users" element={<UserManagement />} />
+//         <Route path="/users/add" element={<AddUser />} />
+//         <Route path="/users/:userId/edit" element={<EditUser/>}/>
+//         <Route path="/branch_profile/:branchId" element={<BranchProfile />} />
+//         <Route path="/branch_profile/:branchId/edit" element={<BranchProfileEdit />} />
+
+
+//         <Route path="*" element={<Navigate to="/" />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
 
 
 
