c857c4b7d1297485b5c2ce27b4e859c78ba043cc
Maneesha-Balasooriya <maneesha.bsc.se02@edu.lnbti.lk>
2026-05-25 14:35:44 +0530

    New Working Branch Management on WebSockets

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -44,13 +44,6 @@ export const getBranches = async () => {
   return response.data;
 };
 
-
-
-export const updateBranch = async (id, branchData) => {
-  const response = await api.put(`/branches/${id}`, branchData);
-  return response.data;
-};
-
 export const deleteBranch = async (id) => {
   const response = await api.delete(`/branches/${id}`);
   return response.data;
