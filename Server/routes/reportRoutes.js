2eedc9511626730cf36e09b7b2c24715fc94e9dd
YasithMadhushan <yasithmadhushan416@gmail.com>
2026-06-14 20:21:39 +0530

    add product sales reporting

--- Server/routes/reportRoutes.js
+++ Server/routes/reportRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import {getSalesSummaryReport} from "../controllers/reportController.js";
+import {
+    getSalesSummaryReport,
+    getProductSalesReport
+} from "../controllers/reportController.js";
 
 const router = express.Router();
 
 router.post("/sales", getSalesSummaryReport);
+router.post("/productsales",getProductSalesReport);
 
 export default router;
