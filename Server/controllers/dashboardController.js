01a2ad1f18f90b4219be683131ce37b808539046
YasithMadhushan <yasithmadhushan416@gmail.com>
2026-09-10 16:18:05 +0530

    feat: Refactor order retrieval logic to filter completed and paid orders in dashboards

--- Server/controllers/dashboardController.js
+++ Server/controllers/dashboardController.js
@@ -14,35 +14,39 @@ export async function getCashierDashboardStats(req, res) {
     const query = `
       SELECT
           (
-              SELECT COALESCE(SUM("or_totalCostWtax"), 0)
-              FROM "ORDER"
-              WHERE "or_status" = 'completed'
-                AND "or_date"= CURRENT_DATE
-                AND "b_id" = $1
+              SELECT COALESCE(SUM(p."pay_amount"), 0)
+              FROM "Payment" p
+              JOIN "ORDER" o ON p."or_id" = o."or_id"
+              WHERE p."pay_status" = 'paid'
+                AND p."pay_date" = CURRENT_DATE
+                AND o."b_id" = $1
           ) AS revenue,
 
           (
-              SELECT COUNT(*)
-              FROM "ORDER"
-              WHERE "or_status" = 'completed'
-                AND "or_date" = CURRENT_DATE
-                AND "b_id" = $1
+              SELECT COUNT(DISTINCT o."or_id")
+              FROM "ORDER" o
+              LEFT JOIN "Payment" p ON p."or_id" = o."or_id"
+              WHERE (o."or_status" = 'completed' OR p."pay_status" = 'paid')
+                AND (o."or_date" = CURRENT_DATE OR p."pay_date" = CURRENT_DATE)
+                AND o."b_id" = $1
           ) AS transactions,
 
           (
-              SELECT COUNT(DISTINCT "cust_id")
-              FROM "ORDER"
-              WHERE "or_status" = 'completed'
-                AND "or_date" = CURRENT_DATE
-                AND "b_id" = $1
+              SELECT COUNT(DISTINCT o."cust_id")
+              FROM "ORDER" o
+              LEFT JOIN "Payment" p ON p."or_id" = o."or_id"
+              WHERE (o."or_status" = 'completed' OR p."pay_status" = 'paid')
+                AND (o."or_date" = CURRENT_DATE OR p."pay_date" = CURRENT_DATE)
+                AND o."b_id" = $1
           ) AS total_customers,
 
           (
               SELECT COALESCE(SUM(oi.pro_quantity), 0)
               FROM "ORDER_ITEM" oi
               JOIN "ORDER" o ON oi."order_id" = o."or_id"
-              WHERE o."or_status" = 'completed'
-                AND o."or_date" = CURRENT_DATE
+              LEFT JOIN "Payment" p ON p."or_id" = o."or_id"
+              WHERE (o."or_status" = 'completed' OR p."pay_status" = 'paid')
+                AND (o."or_date" = CURRENT_DATE OR p."pay_date" = CURRENT_DATE)
                 AND o."b_id" = $1
           ) AS products_sold
     `;
